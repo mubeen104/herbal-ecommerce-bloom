1b00cf50cce13a175252cda19810cac5e95e29f7
mubeen104 <135359737+mubeen104@users.noreply.github.com>
2025-07-31 21:59:45 +0500

    chng2

--- src/components/FeaturedProducts.tsx
+++ src/components/FeaturedProducts.tsx
@@ -117,7 +117,7 @@ const FeaturedProducts = () => {
         </div>
 
         {/* Modern Products Grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 lg:gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 sm:gap-8 lg:gap-10 px-4 sm:px-6 lg:px-8">
           {products.map((product, index) => (
             <div
               key={product.id}
